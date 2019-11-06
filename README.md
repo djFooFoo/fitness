4a237dda4be11a2ec8a0a8f76004ff8527c24317
Dieter <dieter.jordens@continuum.be>
2019-11-06 20:02:55 +0100

    add readme.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # fitness
 This will give an example on how the new modules work in combination with the Service Loader API. I've targetted Java 11.
+
+# Fitness
+
+Fitness is an example on how to use the Service Loader API combined with modules and maven.
+
+## Installation
+
+Run a maven clean install.
+
+```bash
+mvn clean install
+```
+
+Afterwards run generation.main.Main to view the output
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to add tests when you are adding any changes.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- main/src/main/java/generation/main/Main.java
+++ main/src/main/java/generation/main/Main.java
@@ -12,14 +12,16 @@ public class Main {
 	public static void main(String[] args) {
 		// We don't specify where our implementation is coming from. This is not decided at compile time, but at run time.
 		// Notice that we do specify the runtime dependency in the pom.xml, so that's where the implementations come from!
-		ServiceLoader<GenerationService> generationServices = ServiceLoader.load(GenerationService.class);
 
-		System.out.println("We found " + generationServices.stream().count() + " generation services.");
+		ServiceLoader.load(GenerationService.class)
+				.stream()
+				.map(ServiceLoader.Provider::get)
+				.map(GenerationService::generateSchedule)
+				.forEach(Main::printSchedule);
+	}
 
-		for(GenerationService generationService: generationServices){
-			Schedule schedule = generationService.generateSchedule();
-			System.out.println(schedule.getName() + " with level: " + schedule.getLevel());
-		}
+	private static void printSchedule(Schedule schedule) {
+		System.out.println(schedule.getName() + " with level: " + schedule.getLevel());
 	}
 
 }
